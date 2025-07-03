ab645496d3956b7c9803dc45d27ee43a5fbff721
Sushi-Mampfer <sushimampfer@duck.com>
2025-07-03 18:55:14 +0200

    basic functionality done

---
+++ README.md
@@ -0,0 +1,8 @@
+# Clip
+`clip` is a command line tool to manage your clipboard, you can pipe something into it and out of it.
+
+### Roadmap
+- [x] Linking to system clipboard and piping in and out
+- [ ] Sharing
+- [ ] History
+- [ ] Syncing
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use std::{io::{self, IsTerminal, Read, Write}, process::exit};
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about = "A very simple cli tool to use the clipboard from terminal, pipe into it to store and run without any args to get the value.")]
+struct Cli {
+/*     #[arg(short, long, help = "If set a link to the clipboard will be created.")]
+    share: bool,
+ */
+    #[arg(help = "Value to set the clipboard to")]
+    value: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    let mut clipboard = clippers::Clipboard::get();
+    let mut stdin = io::stdin();
+    let mut stdout = io::stdout();
+    let mut stderr = io::stderr();
+
+    match cli.value {
+        Some(data) => {
+            clipboard.write_text(data).unwrap();
+            exit(0)
+        }
+        _ => ()
+    }
+
+    if stdin.is_terminal() {
+        match clipboard.read() {
+            Some(data) => {
+                match data.as_text() {
+                    Some(text) => stdout.write_all(text.as_bytes()).unwrap(),
+                    None => stderr.write_all(b"No string in clipboard").unwrap(),
+                }
+            }
+            None => ()
+        }
+        exit(0)
+    }
+    let mut data = String::new();
+    stdin.read_to_string(&mut data).unwrap();
+    clipboard.write_text(data).unwrap();
 }
